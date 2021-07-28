a03370c25df9bb8c60dc34760fe12f63cfd6010f
yuri-andrade97 <dev.yuri97@gmail.com>
2021-07-28 07:52:41 -0300

    crud f criado com sucesso

---
+++ controladores/livros.js
@@ -0,0 +1,117 @@
+const conexao = require('../conexao');
+
+const listarLivros = async (req, res) => {
+  try {
+    const query = `
+      SELECT livros.id, autores.nome as nome_autor, livros.nome, livros.genero, livros.editora, livros.data_publicacao FROM livros
+      LEFT JOIN autores ON livros.autor_id = autores.id
+    `
+    const { rows: livros } = await conexao.query(query);
+
+    return res.status(200).json(livros);
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+};
+
+const obterLivro = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const livro = await conexao.query('select * from livros where id = $1', [id]);
+
+    if(livro.rowCount === 0) {
+      return res.status(404).json('Livro não encontrado')
+    }
+
+    return res.status(200).json(livro.rows);
+
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+};
+
+const cadastrarLivro = async (req, res) => {
+  const { 
+    autor_id, nome, editora, genero, data_publicacao
+  } = req.body;
+
+  // verificação campo autor_id e genero
+  if (!genero) {
+    return res.status(400).json("O campo genero é obrigatório");
+  }
+
+  if (!autor_id) {
+    return res.status(400).json("O campo ID do Autor é obrigatório");
+  }
+
+  try {
+    const livroCadastrado = await conexao.query('insert into livros (autor_id, nome, editora, genero, data_publicacao) values ($1, $2, $3, $4, $5)', [autor_id, nome, editora, genero, data_publicacao]);
+
+    if (livroCadastrado.rowCount === 0) {
+      return res.status(400).json('Não foi possível cadastrar o livro');
+    } 
+
+    return res.status(200).json('Livro cadastrado com sucesso.');
+
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+};
+
+const atualizarLivro = async (req, res) => {
+  const { id } = req.params;
+  const {
+     autor_id, nome, editora, genero, data_publicacao
+   } = req.body;
+
+  try {
+    const livro = await conexao.query('select * from livros where id = $1', [id]);
+    
+    if (livro.rowCount === 0) {
+      return res.status(404).json('livro não encontrado')
+    }
+
+    if (!nome) {
+      return res.status(400).json("O campo nome é obrigatório.")
+    }
+
+    if (!autor_id) {
+      return res.status(400).json("O campo id do autor é obrigatório.")
+    }
+
+    const livroAtualizado = await conexao.query('update livros set autor_id = $1, nome = $2, editora = $3, genero = $4, data_publicacao = $5 where id = $6', [autor_id, nome, editora, genero, data_publicacao, id]);
+
+    if (livroAtualizado.rowCount === 0) {
+      return res.status(404).json('Não foi possível atualizar o livro')
+    }
+
+    return res.status(200).json('Livro atualizado com sucesso.');
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+};
+
+const excluirLivro = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const livroExcluido = await conexao.query('delete from livros where id = $1', [id]);
+
+    if (livroExcluido.rowCount === 0) {
+      return res.status(404).json('Livro não encontrado.');
+    }
+
+    return res.status(200).json('Livro excluido com sucesso.');
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+};
+
+module.exports = {
+  listarLivros,
+  obterLivro,
+  cadastrarLivro,
+  atualizarLivro,
+  excluirLivro
+}
---
+++ controladores/usuarios.js
@@ -0,0 +1,137 @@
+const conexao = require('../conexao');
+
+
+const listarUsuarios = async (req, res) => {
+  try {
+    const query = `
+    select * from usuarios
+  `;
+  const { rows: usuarios } = await conexao.query(query);
+  return res.status(200).json(usuarios);
+
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+};
+
+const obterUsuario = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const query = `
+      select * from usuarios where id = $1
+    `;
+    const usuario = await conexao.query(query, [id]);
+
+    if (usuario.rowCount === 0) {
+      res.status(404).json('Usuário não encontrado.')
+    }
+    return res.status(200).json(usuario.rows);
+
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+};
+
+const cadastrarUsuario = async (req, res) => {
+  const {
+    nome, idade, email, telefone, cpf
+  } = req.body;
+
+  try {
+   const query = `
+      insert into usuarios
+      (nome, idade, email, telefone, cpf)
+      values
+      ($1, $2, $3, $4, $5)
+   `;
+
+   if(!nome) {
+    return res.status(400).json("O campo nome é obrigatório.");
+   }
+
+   if(!email) {
+    return res.status(400).json("O campo email é obrigatório.");
+   }
+
+   if(!cpf) {
+    return res.status(400).json("O campo cpf é obrigatório.");
+   }
+
+   const usuarioCadastrado = await conexao.query(query, [nome, idade, email, telefone, cpf]);
+
+   if (usuarioCadastrado.rowCount === 0) {
+    return res.status(400).json('Não foi possível cadastrar o autor');
+  }
+
+   return res.status(200).json('Usuário cadastrado com sucesso.');
+
+  } catch (error) {
+   return res.status(400).json(error.message);
+  }
+};
+
+const atualizarUsuario = async (req, res) => {
+  const { id } = req.params;
+  const {
+    nome, idade, email, telefone, cpf
+  } = req.body;
+
+  try {
+    const query = `
+    update usuarios set nome = $1, idade = $2, email = $3, telefone = $4, cpf = $5
+    where id = $6
+    `;
+
+    if(!nome) {
+      return res.status(400).json("O campo nome é obrigatório.");
+     }
+  
+     if(!email) {
+      return res.status(400).json("O campo email é obrigatório.");
+     }
+  
+     if(!cpf) {
+      return res.status(400).json("O campo cpf é obrigatório.");
+     }
+
+    const usuarioAtualizado = await conexao.query(query, [nome, idade, email, telefone, cpf, id])
+
+    if (usuarioAtualizado.rowCount === 0) {
+      return res.status(400).json('Não foi possível atualizar o autor');
+    }
+
+    return res.status(200).json('Usuário atualizado com sucesso')
+
+    
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+};
+
+const excluirUsuario = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const query = `
+      delete from usuarios where id = $1
+    `;
+
+    const usuarioExcluido = await conexao.query(query, [id]);
+
+    if (usuarioExcluido.rowCount === 0) {
+      return res.status(404).json('Não foi possível excluir o usuário');
+    }
+    res.status(200).json('usuário excluido com sucesso')
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+};
+
+module.exports = {
+  listarUsuarios,
+  obterUsuario,
+  cadastrarUsuario,
+  atualizarUsuario,
+  excluirUsuario
+}
